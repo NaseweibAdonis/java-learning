5f8efe994232f1dc404a2d42858104c0066bb78f
Raveendhar <raveendhar2007@gmail.com>
2026-07-25 23:31:11 +0530

    Add GradeEvaluator.java

---
+++ ProblemsSet1/GradeEvaluator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class GradeEvaluator {
+    static void gradeEvaluater(int marks){
+        if(marks >= 90){
+            System.out.println("Grade A");
+        }
+        else if(marks >= 75 && marks <= 89){
+            System.out.println("Grade B");
+        }
+        else if(marks >= 50 && marks <= 74){
+            System.out.println("Grade C");
+        }
+        else{
+            System.out.println("Grade F");
+        }
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the mark: ");
+        int mark = sc.nextInt();
+
+        gradeEvaluater(mark);
+    }
+}
