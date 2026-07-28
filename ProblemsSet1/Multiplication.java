e7e64fe2db07d08044056f6f9a92dec7a0bc0acf
Raveendhar <raveendhar2007@gmail.com>
2026-07-28 09:34:24 +0530

    Add Multiplication.java

---
+++ ProblemsSet1/Multiplication.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Multiplication{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        System.out.println("Multiplication table of " + n );
+
+        for(int i = 1; i <= 10; i++){
+                System.out.println( n + " x " +  i + " = " + (n * i));
+        }
+    } 
+}
