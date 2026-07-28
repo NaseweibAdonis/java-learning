d62bf3f2fb3f4332c365e433cdf82df6fc6fef6c
Raveendhar <raveendhar2007@gmail.com>
2026-07-28 09:32:41 +0530

    Add EvenNumbersinaRange.java

---
+++ ProblemsSet1/EvenNumbersinaRange.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenNumbersinaRange {
+    public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter the value to find the even numbers: ");
+            int n = sc.nextInt();
+
+             System.out.println("The even numbers are: ");
+            for(int i = 1; i <= n; i++){
+                if(i % 2 == 0){
+                   System.out.println(i);
+                }
+            }
+    }
+}
