0a9ed01a9fa05647f7ec83b0ddd6e6d242a55479
Raveendhar <raveendhar2007@gmail.com>
2026-07-28 11:33:34 +0530

    Add CountdownTimer.java

--- ProblemsSet1/CountdownTimer.java
+++ ProblemsSet1/CountdownTimer.java
@@ -1,5 +1,12 @@
 //Start from 10 and count down to 1, then print "Liftoff!".
 
 public class CountdownTimer {
-    
+    public static void main(String[] args) {
+        int n = 10;
+        while(n >= 0){
+            System.out.print(n + " ");
+            n--;
+        }
+        System.out.println("Liftoff!");
+    }
 }
