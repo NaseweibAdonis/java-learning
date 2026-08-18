274f0caa0b1df5fbce685cf8f72a56d706880d2f
Raveendhar <raveendhar2007@gmail.com>
2026-08-18 12:38:26 +0530

    Add

--- ProblemSet2/while-loop/BinarytoDecimalConversion.java
+++ ProblemSet2/while-loop/BinarytoDecimalConversion.java
@@ -3,12 +3,8 @@
 import java.util.Scanner;
 public class BinarytoDecimalConversion {
 
-    // public static int binarytoDecimalConversion(int n){
-    // }
-    public static void main(String[] args) {
-        
-    
-    int binary = 101001;
+  public static void binarytoDecimalConversion(int binary) {
+
     int rem = 0;
     double counter = 0;
     double decimal = 0.0;
@@ -21,5 +17,14 @@ public class BinarytoDecimalConversion {
     }
         int final_decimal = (int) (decimal);
         System.out.println(final_decimal);
+
+      }
+    
+    public static void main(String[] args) {        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a binary number: ");
+        int binary = sc.nextInt();
+
+        binarytoDecimalConversion(binary);
 }
 }
